687bdd647e13cb5e7f5fb1247c6cc2c5cae5a66b
Kostiantyn Korniienko <kostiantyn.korniienko@trilogy.com>
2022-12-19 14:06:49 +0200

    introducing counter storage

--- src/background.ts
+++ src/background.ts
@@ -6,67 +6,9 @@
 // See https://developer.chrome.com/extensions/background_pages
 
 import { SlaEvent, SlaEventType } from './models';
-import { isPresent } from 'ts-is-present';
-
-const counterStorage = {
-  get: (cb: (count: string) => void) => {
-    chrome.storage.sync.get(['count'], (result) => {
-      cb(result.count);
-    });
-  },
-  set: (value: string, cb?: () => void) => {
-    chrome.storage.sync.set(
-      {
-        count: value
-      },
-      () => {
-        if(isPresent(cb)) {
-          cb();
-        }
-      }
-    );
-  },
-};
-
-function setupCounter(initialValue = 'n/a') {
-  chrome.action.setBadgeText({
-    text: initialValue.toString()
-  });
-}
-
-function updateCounter(event: SlaEvent) {
-  counterStorage.set(event.payload.message)
-  chrome.action.setBadgeText({
-    text: event.payload.message
-  });
-  if(event.payload.message === '5ₘ0ₛ' || event.payload.message === '1ₘ0ₛ') {
-    chrome.notifications.create('', {
-      title: 'Hurry up',
-      message: event.payload.message,
-      iconUrl: '/icons/icon_128.png',
-      type: 'basic'
-    });
-  }
-  chrome.action.setBadgeBackgroundColor({
-    color: event.payload.color ?? 'rgb(84, 177, 133)'
-  });
-}
+import { restoreCounter, updateCounter } from './counterStorage';
 
 restoreCounter();
-
-function restoreCounter() {
-  // Restore count value
-  counterStorage.get((count: string) => {
-    if (typeof count === 'undefined') {
-      // Set counter value as 0
-      counterStorage.set('n/a', () => {
-        setupCounter();
-      });
-    } else {
-      setupCounter(count);
-    }
-  });
-}
 
 chrome.runtime.onMessage.addListener((request: SlaEvent, sender, sendResponse) => {
   let message;
