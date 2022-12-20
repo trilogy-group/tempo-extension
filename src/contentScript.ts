6b0c88c4730e2456275a6546e354621f7f3c49f1
Kostiantyn Korniienko <kostiantyn.korniienko@trilogy.com>
2022-12-20 18:31:18 +0200

    History (#5)
    * History json
    
    * history details
    
    * bump up version

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,10 +1,35 @@
-import { SlaEvent, SlaEventType } from './models';
+import { HistoryEvent, HistoryObject, SlaEvent, SlaEventType } from './models';
 import $ from 'jquery';
 
-function loadSla() {
+async function getHistory(): Promise<HistoryObject|undefined> {
+  const keys = Object.keys(localStorage).filter(x => x.startsWith('CognitoIdentityServiceProvider.') && x.endsWith('.idToken'));
+  if (keys.length === 0) {
+    console.log("No token id found")
+    return undefined;
+  }
+  const idToken: string = localStorage.getItem(keys[0])!;
+
+  const result = await fetch("https://api.alp-pulse.com/graphql", {
+    'headers': {
+      "accept": "*/*",
+      "authorization": idToken,
+      "content-type": "application/json",
+    },
+    "body": "{\"operationName\":\"CurrentTasks\",\"variables\":{},\"query\":\"query CurrentTasks {\\n  currentTasks {\\n    task {\\n      ...Task\\n      __typename\\n    }\\n    history {\\n      ...Task\\n      __typename\\n    }\\n    automations {\\n      ...HttpAutomation\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\\nfragment Task on Task {\\n  id\\n  companyId\\n  productId\\n  assemblyLineId\\n  workProduct {\\n    inputArtifactURLs\\n    id\\n    name\\n    __typename\\n  }\\n  workUnitId\\n  workUnit {\\n    name\\n    __typename\\n  }\\n  taskType\\n  priority\\n  sla\\n  name\\n  artifactURLs\\n  inputArtifactURLs\\n  inputQualityBar\\n  description\\n  fetchedAt\\n  fetchedBy\\n  doerWorkerId\\n  doTaskId\\n  doCompletedAt\\n  qcOutcome\\n  status\\n  createdAt\\n  updatedAt\\n  createdBy\\n  updatedBy\\n  completedAt\\n  durationInSeconds\\n  rejectReason\\n  rejectDetails\\n  andonCordPullReason\\n  andonCordPullDetails\\n  andonCordResolution\\n  andonCordRestartALName\\n  __typename\\n}\\n\\nfragment HttpAutomation on HttpAutomation {\\n  andonCordPullReason\\n  andonCordPullDetails\\n  fetchedAt\\n  httpStatus\\n  url\\n  workProductId\\n  __typename\\n}\"}",
+    "method": "POST",
+    // "credentials": "include"
+  });
+
+  return await result.json()
+}
+
+let hasHistory = false;
+let lastTitle: string | undefined = undefined;
+
+async function loadSla() {
   const pullWork = $('button:contains("Pull Work")');
   let event: SlaEvent;
-  if(pullWork.length > 0) {
+  if (pullWork.length > 0) {
     event = {
       type: SlaEventType.New,
       payload: {
@@ -23,6 +48,10 @@ function loadSla() {
 
     const mainSection = $("#__next > div > div > div > div > div > div > span");
     const title = mainSection?.get(0)?.textContent ?? ''
+    if(lastTitle !== title) {
+      hasHistory = false;
+      lastTitle = title;
+    }
     const description = mainSection?.get(1)?.textContent ?? ''
     event = {
       type: SlaEventType.New,
@@ -35,7 +64,19 @@ function loadSla() {
     }
   }
 
-  chrome.runtime.sendMessage(event,(response) => {});
+  chrome.runtime.sendMessage(event).then();
+  if (!hasHistory) {
+    const result = await getHistory();
+    if(result === undefined) {
+      return;
+    }
+    const authEvent: HistoryEvent = {
+      type: SlaEventType.History,
+      payload: result
+    }
+    chrome.runtime.sendMessage(authEvent).then();
+    hasHistory = true;
+  }
 }
 
 $(() => {
--- src/popup.ts
+++ src/popup.ts
@@ -1,21 +1,51 @@
 'use strict';
 
 import './popup.css';
-import { SlaEvent } from './models';
-import { getSla } from './slaStorage';
+import { CurrentTask, History, HistoryEvent, SlaEvent } from './models';
+import { getHistory, getSla } from './slaStorage';
 
 (function () {
-  function updateUi(event: SlaEvent) {
+  function updateSlaUi(event: SlaEvent) {
     const newCount = event.payload.sla;
     document.getElementById('counter')!.innerHTML = `<div style="color: ${event.payload.color}">${newCount}</div>`;
     document.getElementById('title')!.innerHTML = event.payload.title ?? 'N/A';
     document.getElementById('subtitle')!.innerHTML = event.payload.description ?? 'N/A';
   }
 
+  function renderHistory(history: History): string {
+    return `
+<div class="history-item">
+  <div>Executor: ${history.fetchedBy}</div>
+  <div>Fetched at: ${history.fetchedAt}</div>
+  <div>Completed at: ${history.completedAt}</div>
+  <div>Status: ${history.status}</div>
+  <div>Reject reason: ${history.rejectReason}</div>
+  <div>Reject details: ${history.rejectDetails.replaceAll(/(https?:\/\/\S+)/g,'<a target="_blank" href="$1">$1</a>')}</div>
+  <div>Durations: ${new Date(history.durationInSeconds * 1000).toISOString().slice(11, 19)}</div>
+</div>`
+  }
+
+  function renderHistoryTask(task: CurrentTask): string {
+    return task.history.map(renderHistory).join(' ');
+  }
+
+  function updateHistoryUi(event: HistoryEvent) {
+    const historyItems = '<div  class="history-item">History: </div>' + event.payload.data.currentTasks.map(renderHistoryTask).join(' ');
+    const historyElement = document.getElementById('history')!;
+    const currentContent = historyElement.innerHTML.toString();
+    if(currentContent.replaceAll(/\s/g, "") !== historyItems.replaceAll(/\s/g, "")) {
+      historyElement.innerHTML = historyItems;
+    }
+  }
+
   async function refresh() {
-    const event = await getSla();
-    updateUi(event);
+    const sliEvent = await getSla();
+    updateSlaUi(sliEvent);
+
+    const historyEvent = await getHistory();
+    updateHistoryUi(historyEvent);
   }
 
   setInterval(refresh, 1000);
 })();
+
