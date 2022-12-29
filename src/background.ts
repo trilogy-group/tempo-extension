5b83a1f6ca027227a1ea09e8e7dbad1b966ed143
Kostiantyn Korniienko <kostiantyn.korniienko@trilogy.com>
2022-12-29 13:45:56 +0200

    cleanup

--- src/background.ts
+++ src/background.ts
@@ -95,7 +95,4 @@ chrome.runtime.onInstalled.addListener(function() {
       }
     }
   });
-  // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  //   chrome.tabs.reload(tabs[0].id);
-  // });
 });
